4c99d2af109d10eee085d4abade4d7f973b85c8f
osama32895 <msa32895@gmail.com>
2025-05-05 13:46:22 +0300

    update

--- 3- start.bat
+++ 3- start.bat
@@ -1,7 +1,24 @@
 @echo off
+cls
+
+echo.
 echo === Pulling latest changes before launching Godot ===
 git pull origin main
 
+echo.
 echo === Starting Godot Engine ===
-start "" "%USERPROFILE%\Desktop\Godot.exe" "%USERPROFILE%\Desktop\project.godot"
+start /wait "" "%USERPROFILE%\Desktop\Godot.exe" "%USERPROFILE%\Desktop\breakers-game - Copy\breaker-game-project"
+
+echo.
+echo === Adding all changes... ===
+git add .
+
+set commitMsg=update
+
+git commit -m "%commitMsg%"
+
+echo.
+echo === Pushing to GitHub... ===
+git push origin main
+
 pause
